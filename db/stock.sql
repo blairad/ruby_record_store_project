15469cf1c65f3a44ce6be03344a9477c99fa2f93
Andrew Blair <blair.d.andrew@gmail.com>
2019-04-29 11:38:17 +0100

    removed table of stock

--- db/stock.sql
+++ db/stock.sql
@@ -1,4 +1,4 @@
-DROP TABLE stock;
+-- DROP TABLE stock;
 DROP TABLE albums;
 DROP TABLE artists;
 
@@ -6,14 +6,17 @@ CREATE TABLE artists (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR (255)
 );
+
 CREATE TABLE albums (
   id SERIAL8 PRIMARY KEY,
   title VARCHAR (255),
   stock INT8,
-  artist_id INT8 REFERENCES artists(id) 
+  artist_id INT8 REFERENCES artists(id),
 );
-CREATE TABLE stock (
-  id SERIAL8 PRIMARY KEY,
-  artist_id INT8 REFERENCES artists(id) ON DELETE CASCADE,
-  album_id INT8 REFERENCES albums(id) ON DELETE CASCADE
-);
+
+
+-- CREATE TABLE stock (
+--   id SERIAL8 PRIMARY KEY,
+--   artist_id INT8 REFERENCES artists(id) ON DELETE CASCADE,
+--   album_id INT8 REFERENCES albums(id) ON DELETE CASCADE
+-- );
